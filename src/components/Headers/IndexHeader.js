5ca031da137992536479165e10db5557916953fe
rueangrit-sri <112143671+rueangrit-sri@users.noreply.github.com>
2024-04-21 17:32:28 +0700

    background edit

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -1,51 +1,102 @@
-/*eslint-disable*/
-import React from "react";
+// /*eslint-disable*/
+// import React from "react";
 
-// reactstrap components
+// // reactstrap components
+// import { Container } from "reactstrap";
+// // core components
+
+// function IndexHeader() {
+//   let pageHeader = React.createRef();
+
+//   React.useEffect(() => {
+//     if (window.innerWidth > 991) {
+//       const updateScroll = () => {
+//         let windowScrollTop = window.pageYOffset / 3;
+//         pageHeader.current.style.transform =
+//           "translate3d(0," + windowScrollTop + "px,0)";
+//       };
+//       window.addEventListener("scroll", updateScroll);
+//       return function cleanup() {
+//         window.removeEventListener("scroll", updateScroll);
+//       };
+//     }
+//   });
+
+//   return (
+//     <>
+//       <div className="page-header clear-filter" filter-color="">
+//         <div
+//           className="page-header-image"
+//           style={{
+//             backgroundImage: "url(" + require("assets/img/down.jpg") + ")"
+//           }}
+//           ref={pageHeader}
+//         ></div>
+//         <Container>
+//           <div className="content-center brand">
+//           <img alt="" src="LogoDUNGDEE.png" width="300" height="300" className="d-inline-block align-top"/>
+//             <h1 className="h1-seo">มาดูโหงวเฮ้งของตัวเองกัน </h1>
+//             <h3>Free for use</h3>
+//           </div>
+//           <h6 className="category category-absolute">
+//             Designed by Duduang Company{" "}
+          
+          
+//             . Coded by Chavalit 
+//             .
+//           </h6>
+        
+//         </Container>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default IndexHeader;
+
+import React, { useMemo } from "react";
 import { Container } from "reactstrap";
-// core components
 
 function IndexHeader() {
   let pageHeader = React.createRef();
+  const [bgImageIndex, setBgImageIndex] = React.useState(0);
+
+  const images = useMemo(() => [
+    require("assets/img/down.jpg"),
+    require("assets/img/down2.jpg"),
+    require("assets/img/down3.jpg"),
+    // เพิ่ม URL ของภาพพื้นหลังเพิ่มเติมตามต้องการ
+  ], []);
 
   React.useEffect(() => {
-    if (window.innerWidth > 991) {
-      const updateScroll = () => {
-        let windowScrollTop = window.pageYOffset / 3;
-        pageHeader.current.style.transform =
-          "translate3d(0," + windowScrollTop + "px,0)";
-      };
-      window.addEventListener("scroll", updateScroll);
-      return function cleanup() {
-        window.removeEventListener("scroll", updateScroll);
-      };
-    }
-  });
+    const updateBgImage = () => {
+      const newIndex = (bgImageIndex + 1) % images.length;
+      pageHeader.current.style.backgroundImage = `url(${images[newIndex]})`;
+      setBgImageIndex(newIndex);
+    };
+
+    const interval = setInterval(updateBgImage, 3000); // เปลี่ยนภาพพื้นหลังทุก 5 วินาที (5000 มิลลิวินาที)
+
+    return () => clearInterval(interval);
+  }, [bgImageIndex, images, pageHeader]);
 
   return (
     <>
       <div className="page-header clear-filter" filter-color="">
         <div
           className="page-header-image"
-          style={{
-            backgroundImage: "url(" + require("assets/img/down.jpg") + ")"
-          }}
+          style={{ backgroundSize: "cover", backgroundPosition: "center", transition: "background-image 1s ease-in-out" }}
           ref={pageHeader}
         ></div>
         <Container>
           <div className="content-center brand">
-          <img alt="" src="LogoDUNGDEE.png" width="300" height="300" className="d-inline-block align-top"/>
+            <img alt="" src="LogoDUNGDEE.png" width="300" height="300" className="d-inline-block align-top"/>
             <h1 className="h1-seo">มาดูโหงวเฮ้งของตัวเองกัน </h1>
             <h3>Free for use</h3>
           </div>
           <h6 className="category category-absolute">
-            Designed by Duduang Company{" "}
-            
-            
-            . Coded by Chavalit 
-            .
+            Designed by Duduang Company . Coded by Chavalit .
           </h6>
-          
         </Container>
       </div>
     </>
@@ -53,3 +104,4 @@ function IndexHeader() {
 }
 
 export default IndexHeader;
+
