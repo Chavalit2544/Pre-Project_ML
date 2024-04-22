857a2ffdc25136a459df9dd5cab8e95a33934829
rueangrit-sri <112143671+rueangrit-sri@users.noreply.github.com>
2024-04-22 14:01:22 +0700

    Fade
    ติดตั้ง npm install react-transition-group

--- src/views/examples/LoginPage.js
+++ src/views/examples/LoginPage.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import {CSSTransition } from 'react-transition-group';
+import './fade.css'
 // reactstrap components
 import {
   Button,
@@ -42,60 +43,66 @@ function LoginPage() {
         ></div> <br />  
         <Container>
           <h1>About Us</h1>
-          <Row>
-            <Col>
-              <Card style={{ width: "20rem" , height: "30rem"}}>
-                <CardImg alt="..." src="face.jpg" top style={{ height: "200px", objectFit: "cover"}}></CardImg>
-                <CardBody>
-                  <CardText tag="h6" style={{color:"black"}}>
-                      โหงวเฮ้งเป็นศาสตร์จีนโบราณที่ใช้ในการพิจารณาดูใบหน้า เพื่อวิเคราะห์ถึงบุคลิกและลักษณะนิสัยของแต่ละคน โดยการอ่านจากลักษณะใบหน้าและอริยาบถต่างๆทั้งการพูด น้ำเสียง ไปจนถึงการดูราศี
-                  </CardText>
-                  <Button
-                    color="primary"
-                    href="https://peaceclinicpty.com/lifestyle-physiognomy/"
-                  >
-                    อ่านเพิ่มเติม
-                  </Button>
-                </CardBody>
-              </Card>
-            </Col>
-            
-            <Col>
-              <Card style={{ width: "20rem" , height: "30rem"}}>
-                <CardImg alt="..." src="thairat.jpg" top style={{ height: "200px", objectFit: "cover"}}></CardImg>
-                <CardBody>
-                  <CardTitle tag="h5" style={{color:"black"}}>รูปหน้า" บอก "นิสัย" พร้อมเคล็ดลับความงามสำหรับสาวทุกรูปหน้า</CardTitle>
-                    <CardText tag="h6" style={{color:"black"}}>
-                      โบราณว่า "รู้หน้าไม่รู้ใจ" แต่รู้หรือไม่ว่า "ใบหน้า" สามารถบ่งบอกนิสัยของเราได้ ชาวจีนได้ศึกษาเรื่องนี้มากว่า 3,000 ปี หรือที่เรียกว่า "ศาสตร์โหงวเฮ้ง" นั่นเอง
-                    </CardText>
-                  <Button
-                    color="primary"
-                    href="https://www.thairath.co.th/lifestyle/woman/1780295"
-                  >
-                    อ่านเพิ่มเติม
-                  </Button>
-                </CardBody>
-              </Card>
-            </Col>
+          <CSSTransition in={true} appear={true} timeout={1000} classNames="fade-up">
+            <Row>
+              <Col>
+                <Card style={{ width: "20rem" , height: "30rem" }}>
+                  <CardImg alt="..." src="face.jpg" top style={{ height: "260px", objectFit: "cover"}}></CardImg>
+                  <CardBody>
+                    <CardTitle tag="h5" style={{color:"black"}}> โหงวเฮ้งแบบไหนถึงเรียกว่าดี? มาดูกัน!</CardTitle>
+                      <CardText tag="h6" style={{color:"black"}}>
+                          โหงวเฮ้งเป็นศาสตร์จีนโบราณที่ใช้ในการพิจารณาดูใบหน้า เพื่อวิเคราะห์ถึงบุคลิกและลักษณะนิสัยของแต่ละคน โดยการอ่านจากลักษณะใบหน้าและอริยาบถต่างๆทั้งการพูด น้ำเสียง ไปจนถึงการดูราศี
+                      </CardText>
+                      <Button
+                        color="primary"
+                        href="https://peaceclinicpty.com/lifestyle-physiognomy/"
+                        className="mt-auto"
+                      >
+                        อ่านเพิ่มเติม
+                      </Button>
+                  </CardBody>
+                </Card>
+              </Col>
 
-            <Col>
-              <Card style={{ width: "20 rem" , height: "30rem"}}>
-                <CardImg alt="..." src="faces.png" top style={{ height: "200px", objectFit: "cover"}}></CardImg>
-                <CardBody>
-                  <CardTitle tag="h5" style={{color:"black"}}>วิเคราะห์โหงวเฮ้งจากรูปทรงใบหน้า</CardTitle>
-                  <CardText tag="h6" style={{color:"black"}}>
-                    รูปหน้าพื้นฐานของคุณเป็นสิ่งที่มีประโยชน์มากที่ควรรู้ มันสามารถช่วยคุณเลือกแว่นตาหมวกและแน่นอนรูปทรงคิ้ว มีหมวดหมู่รูปร่างพื้นฐานหกประเภทที่พวกเราส่วนใหญ่ควรพอดีหรือเอนเข้าหา: วงรี กลม ยาว หัวใจ เพชร และสี่เหลี่ยม
-                  </CardText>
-                  <Button
-                    color="primary"
-                    href="https://helenajones.co.uk/do-you-know-your-face-shape/"
-                  >
-                    อ่านเพิ่มเติม
-                  </Button>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
+              
+                <Col>
+                  <Card style={{ width: "20rem" , height: "30rem" }}>
+                    <CardImg alt="..." src="thairat.jpg" top style={{ height: "240px", objectFit: "cover"}}></CardImg>
+                    <CardBody>
+                      <CardTitle tag="h5" style={{color:"black"}}>รูปหน้า" บอก "นิสัย" พร้อมเคล็ดลับความงามสำหรับสาวทุกรูปหน้า</CardTitle>
+                        <CardText tag="h6" style={{color:"black"}}>
+                          โบราณว่า "รู้หน้าไม่รู้ใจ" แต่รู้หรือไม่ว่า "ใบหน้า" สามารถบ่งบอกนิสัยของเราได้ ชาวจีนได้ศึกษาเรื่องนี้มากว่า 3,000 ปี หรือที่เรียกว่า "ศาสตร์โหงวเฮ้ง" นั่นเอง
+                        </CardText>
+                      <Button
+                        color="primary"
+                        href="https://www.thairath.co.th/lifestyle/woman/1780295"
+                      >
+                        อ่านเพิ่มเติม
+                      </Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+              
+
+              <Col>
+                <Card style={{ width: "20 rem" , height: "30rem" }}>
+                  <CardImg alt="..." src="faces.png" top style={{ height: "240px", objectFit: "cover"}}></CardImg>
+                  <CardBody>
+                    <CardTitle tag="h5" style={{color:"black"}}>วิเคราะห์โหงวเฮ้งจากรูปทรงใบหน้า</CardTitle>
+                    <CardText tag="h6" style={{color:"black"}}>
+                      รูปหน้าพื้นฐานของคุณเป็นสิ่งที่มีประโยชน์มากที่ควรรู้ มันสามารถช่วยคุณเลือกแว่นตาหมวกและแน่นอนรูปทรงคิ้ว มีหมวดหมู่รูปร่างพื้นฐานหกประเภทที่พวกเราส่วนใหญ่ควรพอดีหรือเอนเข้าหา: วงรี กลม ยาว หัวใจ เพชร และสี่เหลี่ยม
+                    </CardText>
+                    <Button
+                      color="primary"
+                      href="https://helenajones.co.uk/do-you-know-your-face-shape/"
+                    >
+                      อ่านเพิ่มเติม
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </CSSTransition>
         </Container>
       </div>
       <DarkFooter />
